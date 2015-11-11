71a9620c62d456d919969eea0f3364c2e842fb65
Nikita Tarasov <nikita@mygento.ru>
2015-11-11 12:37:26 +0300

    remove admin store from grid

--- app/code/community/DC/Catalog/Model/Mysql4/Manufacturer/Collection.php
+++ app/code/community/DC/Catalog/Model/Mysql4/Manufacturer/Collection.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Dot Collective - Magento Output 2009
  * Find more about Attribute Info Pages:
@@ -16,9 +17,9 @@
  * @copyright  Copyright (c) 2009 Dot Collective SRL http://dot.collective.ro
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
 {
+
     protected $_previewFlag;
 
     protected function _construct()
@@ -41,18 +42,19 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
     protected function _initSelect()
     {
         $allowedAttributes = explode(',', Mage::getStoreConfig('dc_catalog/attributes/selectedattributes'));
-    	parent::_initSelect();
+        parent::_initSelect();
 
         $this->getSelect()
-        	->joinRight(array('aov' => $this->getTable('eav/attribute_option_value')), 'aov.option_id = main_table.attribute_option_id AND aov.store_id = main_table.attribute_value_store_id', array('value_id', 'value', 'store_id', 'option_id'))
-            ->join(     array('ao'  => $this->getTable('eav/attribute_option')),       'ao.option_id = aov.option_id', array())
-            ->join(     array('a'   => $this->getTable('eav/attribute')),              'a.attribute_id = ao.attribute_id', array(
-            																'attribute_code',
-            																'frontend_label',
-            														        //use a false_id column for having unique ids!
-            														        'concat(a.attribute_code, ao.option_id, \'store\', aov.store_id) as false_id'
-            															))
+            ->joinRight(array('aov' => $this->getTable('eav/attribute_option_value')), 'aov.option_id = main_table.attribute_option_id AND aov.store_id = main_table.attribute_value_store_id', array('value_id', 'value', 'store_id', 'option_id'))
+            ->join(array('ao' => $this->getTable('eav/attribute_option')), 'ao.option_id = aov.option_id', array())
+            ->join(array('a' => $this->getTable('eav/attribute')), 'a.attribute_id = ao.attribute_id', array(
+                'attribute_code',
+                'frontend_label',
+                //use a false_id column for having unique ids!
+                'concat(a.attribute_code, ao.option_id, \'store\', aov.store_id) as false_id'
+            ))
             ->where('a.attribute_code IN (?)', $allowedAttributes)
+            ->where('`aov`.`store_id` != ?', Mage_Core_Model_App::ADMIN_STORE_ID)
             ->where('SUBSTRING(aov.value,1,1) != \'-\'')
             ->order('value');
 
@@ -76,10 +78,11 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
 
     protected function _afterLoad()
     {
-    	parent::_afterLoad();
+        parent::_afterLoad();
 
-    	foreach ($this->_items as $object) {
-            if(!$object->getData('identifier'))         $object->setData('identifier', $this->formatUrlKey($object->getValue()));
+        foreach ($this->_items as $object) {
+            if (!$object->getData('identifier'))
+                $object->setData('identifier', $this->formatUrlKey($object->getValue()));
         }
 
         return $this;
@@ -98,11 +101,11 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
         }
 
         if ($allStores && ($store > 0)) {
-	        $this->getSelect()
-		         ->where('(? in (aov.store_id, attribute_value_store_id)) OR (0 in (aov.store_id, attribute_value_store_id))', $store);
+            $this->getSelect()
+                ->where('(? in (aov.store_id, attribute_value_store_id)) OR (0 in (aov.store_id, attribute_value_store_id))', $store);
         } else {
-	        $this->getSelect()
-		         ->where('? in (aov.store_id, attribute_value_store_id)', $store);
+            $this->getSelect()
+                ->where('? in (aov.store_id, attribute_value_store_id)', $store);
         }
         return $this;
     }
@@ -116,7 +119,7 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
     public function addAttributeCodeFilter($attributeCode)
     {
         $this->getSelect()
-        	 ->where('a.attribute_code = ?', $attributeCode);
+            ->where('a.attribute_code = ?', $attributeCode);
         return $this;
     }
 
@@ -128,8 +131,8 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
      */
     public function addEnabledFilter()
     {
-    	//the disabled flag must be set explicit
-    	//if there is no record in values table, it means it the value page is not created yet, so the default is 1
+        //the disabled flag must be set explicit
+        //if there is no record in values table, it means it the value page is not created yet, so the default is 1
         $this->getSelect()->where('coalesce(main_table.is_active,1) > 0');
         return $this;
     }
@@ -143,9 +146,9 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
     public function addFavoritesFilter()
     {
         $this->getSelect()
-        	->where('favorite > 0')
-        	->reset( Zend_Db_Select::ORDER )
-        	->order('favorite asc');
+            ->where('favorite > 0')
+            ->reset(Zend_Db_Select::ORDER)
+            ->order('favorite asc');
         return $this;
     }
 
@@ -158,8 +161,8 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
     public function randomize()
     {
         $this->getSelect()
-    		->reset( Zend_Db_Select::ORDER )
-        	->order('rand()');
+            ->reset(Zend_Db_Select::ORDER)
+            ->order('rand()');
         return $this;
     }
 
@@ -176,6 +179,4 @@ class DC_Catalog_Model_Mysql4_Manufacturer_Collection extends Mage_Core_Model_My
         //echo $this->getSelect();
         return $this;
     }
-
-
 }
